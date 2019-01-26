df2822cc2c728e399dfe61adc09ce05e4053ece6
blitz450 <saurabh.chauhan450@gmail.com>
2019-01-26 18:14:18 +0530

    Updates

---
+++ precache-manifest.37c702d5a6cd5698059393c515047c26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "eec0608ff80827a878d7da11b3b71857",
+    "url": "/hashKittens/static/media/JOKERMAN.eec0608f.TTF"
+  },
+  {
+    "revision": "a3c5c34d1291cf423435",
+    "url": "/hashKittens/static/js/runtime~main.a3c5c34d.js"
+  },
+  {
+    "revision": "411a9a46b629e09b1cbf",
+    "url": "/hashKittens/static/js/main.411a9a46.chunk.js"
+  },
+  {
+    "revision": "5f08a842f767171a2216",
+    "url": "/hashKittens/static/js/1.5f08a842.chunk.js"
+  },
+  {
+    "revision": "411a9a46b629e09b1cbf",
+    "url": "/hashKittens/static/css/main.6298288f.chunk.css"
+  },
+  {
+    "revision": "5f08a842f767171a2216",
+    "url": "/hashKittens/static/css/1.fd02b258.chunk.css"
+  },
+  {
+    "revision": "e2bd9516c512814cb19c29bbc10cca6d",
+    "url": "/hashKittens/index.html"
+  }
+];
