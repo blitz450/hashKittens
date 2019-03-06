2d8afa967ba2173ecccc5c6fca61f2b4a452c5be
blitz450 <saurabh.chauhan450@gmail.com>
2019-03-06 22:19:16 +0530

    pwa

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox=({searchfield, searchchange})=>{
 	return(
 			<div className='pa2'>
 				<input 
+				aria-label='Search Kittens'
 				className='pa3 ba b--green bg-lightestblue'
 				onChange={searchchange} 
 				type='Search' 
